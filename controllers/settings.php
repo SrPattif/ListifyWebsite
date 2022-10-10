3a000ca249d7540cda8e5521dabb1d60337558ec
SrPattif <omininuguxx@gmail.com>
2022-10-10 17:16:45 -0300

    added .gitignore

--- controllers/settings.php
+++ controllers/settings.php
@@ -1,4 +1,5 @@
 <?php
+    $listifyReleased = false;
     $listifyDomain = 'http://localhost';
     $listifyRedirectURI = 'http://localhost/user/';
     $spotifyAPIKey = 'MDBmZThhODg4MzAzNDAwMjkzYTM2YTFmYjgyZWMxNDU6YjdmZmE4MmVhZjI3NDk1OWIyNWZjYWZhYzViNzhlMTE=';
